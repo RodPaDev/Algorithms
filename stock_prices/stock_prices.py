7321772997b34901a992dc6cb50615edc7fb7171
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-02-19 17:10:56 +0100

    finish stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,18 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  max_profit = prices[1] - prices[0]
+  min_price = prices[0]
 
+  i = 1
+  while i < len(prices):
+    if (prices[i] - min_price) > max_profit:
+      max_profit = prices[i] - min_price
+    if prices[i] < min_price:
+      min_price = prices[i]
+    i += 1
+
+  return max_profit
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
